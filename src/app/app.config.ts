96148c93cbea8b6b96c2e3fdab87123893b95a16
Wayne Gakuo <waynejotham@gmail.com>
2026-02-13 06:47:47 +0300

    feat(firebase): Configure Firebase emulators and initialize project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment.development';
+import {connectFunctionsEmulator, getFunctions, provideFunctions} from '@angular/fire/functions';
+
+const app = initializeApp(environment.firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => app),
+    provideFunctions(() => {
+      const functions = getFunctions(app, 'africa-south1');
+      if (isDevMode() && environment.emulator) {
+        console.log(`Connecting to Functions emulator on ${environment.emulator.host}:${environment.emulator.functionsPort}`);
+        connectFunctionsEmulator(functions, environment.emulator.host, environment.emulator.functionsPort);
+      }
+      return functions;
+    })
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCH2S7XvG3en3Gf0WobGX0UQ-P3p863n48",
+    authDomain: "agents-concierge.firebaseapp.com",
+    projectId: "agents-concierge",
+    storageBucket: "agents-concierge.firebasestorage.app",
+    messagingSenderId: "401327296702",
+    appId: "1:401327296702:web:16c5ff6dacc96ce10e8eef",
+    measurementId: "G-KKVMYRG7QC"
+  }
+};
