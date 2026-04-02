f78fc8feec17d7affb4677046906697cc1a21b23
Wayne Gakuo <waynejotham@gmail.com>
2026-04-02 17:49:10 +0300

    feat(maps-loader): update Google Maps API version from 'alpha' to 'beta'

--- src/app/services/core/google-maps-loader/google-maps-loader.service.ts
+++ src/app/services/core/google-maps-loader/google-maps-loader.service.ts
@@ -31,7 +31,7 @@ export class GoogleMapsLoaderService {
             'loadGoogleMaps',
           );
           const { data } = await loadGoogleMaps();
-          setOptions({ key: data.key, v: 'alpha', libraries: ['places'] });
+          setOptions({ key: data.key, v: 'beta', libraries: ['places'] });
           this.initialized = true;
         } catch (error) {
           console.error('Failed to initialize Google Maps via Cloud Function:', error);
